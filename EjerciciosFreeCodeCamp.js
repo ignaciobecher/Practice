46c8263a7b49c39cf8d3f232fc9fb0ceedc9b188
Ignacio <ignacio.becher@gmail.com>
2022-06-02 16:17:40 -0300

    first

---
+++ EjerciciosFreeCodeCamp.js
@@ -0,0 +1,57 @@
+//Funcion para repetir un strin N cantidad de veces
+// function repeatStringNumTimes(str, num) {
+//   let newStr="";
+//   for(let i=0; i<num; i++){
+//       newStr +=str
+//   }
+//   return newStr
+//   }
+  
+//   console.log(repeatStringNumTimes("abc", 3))
+
+//---------------------------------------------------------//
+
+//Funcion que corta un string y si la variable es menor al length total, imprime lo cortado + ...
+// function truncateString(str, num) {
+//     if(str.length>num){
+//       const strCuted= str.slice(0,num)
+//       return strCuted+"..."
+//     }
+//     else{
+//         return str
+//     }
+    
+//   }
+  
+//  console.log(truncateString("A-tisket a-tasket A green and yellow basket", ))
+
+//---------------------------------------------------------//
+
+//funcion para saber si un dato es un boleano
+// function booWho(bool) {
+//    return typeof bool==="boolean"
+//   }
+  
+//   console.log(booWho(null));
+
+  //---------------------------------------------------------//
+
+//Funcion para pasar primera letra de cadda palabra a mayus
+// function titleCase(str) {
+//    const newTitle = str.split(" ");
+//    const updatedTitle = [];
+//    for (let st in newTitle) {
+//      updatedTitle[st] = newTitle[st][0].toUpperCase() + newTitle[st].slice(1).toLowerCase();
+//    }
+//    return updatedTitle.join(" ");
+//  }
+
+  //---------------------------------------------------------//
+
+  function mutation(arr) {
+    for (let i=0; i<arr.length; i++){
+      if(arr[i][0])
+    }
+  }
+  
+  mutation(["hello", "hey"]);
